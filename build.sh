ef2ea9c4da7046c88c1187250dad2659818dfbf5
J. Preiss <joerg.preiss@slesa.de>
2025-10-19 22:15:35 +0200

    Try to get it compiled

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+dotnet tool restore
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  exit $exit_code
+fi
+
 dotnet paket restore
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
--- src/FsReveal/Domain.fs
+++ src/FsReveal/Domain.fs
@@ -4,8 +4,30 @@ open System
 open System.IO
 open System.Collections.Generic
 open System.Text
-open FSharp.Literate
+open FSharp.Formatting
+open FSharp.Markdownnamespace FsReveal
+
+open System
+open System.IO
+open System.Collections.Generic
+open System.Text
+open FSharp.Formatting
 open FSharp.Markdown
+open FSharp.Markdown.Html
+
+type SlideData = 
+    { Properties : Map<string,string>
+      Paragraphs : MarkdownParagraph list }
+
+type Slide = 
+    | Simple of SlideData
+    | Nested of SlideData list
+
+type Presentation = 
+    { Properties : Map<string,string>
+      Slides : Slide list
+      Document : LiterateDocument }
+
 open FSharp.Markdown.Html
 
 type SlideData = 
